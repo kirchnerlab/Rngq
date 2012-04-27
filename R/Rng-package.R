b77e8fcbbb6dbbe9828eb81c1bc590c7d401e5d5
Marc Kirchner <mail@marc-kirchner.de>
2012-04-27 09:37:20 +0200

    initial import, initial roxygen2
    Signed-off-by: Marc Kirchner <mail@marc-kirchner.de>

---
+++ R/Rng-package.R
@@ -0,0 +1,22 @@
+#' Data processing functions for Next Generation Quantification data
+#'
+#' \tabular{ll}{
+#' Package: \tab Rngq\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1\cr
+#' Date: \tab 2012-04-27\cr
+#' License: \tab MIT\cr
+#' }
+#'
+#' This package provides a few function to process the peptide-level
+#' quantifications extracted by NGQ.
+#'
+#' @name Rngq-package
+#' @aliases Rngq
+#' @docType package
+#' @title Process NGQ peptide-level data
+#' @author Marc Kirchner \email{mail@marc-kirchner.de}
+#' @references
+#' \url{http://massquito.net}
+#' @keywords package
+roxygen()
